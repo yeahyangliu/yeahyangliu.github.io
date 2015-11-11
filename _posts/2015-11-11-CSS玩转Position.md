9fcbcbe2196d8b9b1b46e6b90374eafffb57ee80
YANG Liu <yeah_yangliu@163.com>
2015-11-11 13:07:42 +0800

    add img & refactor

--- _posts/2015-11-11-CSS玩转Position.md
+++ _posts/2015-11-11-CSS玩转Position.md
@@ -6,6 +6,8 @@ categories: [前端]
 tags: [CSS]
 
 ---
+![position](/assets/image/css-position.jpg)
+
 <br/>
     布局是前端开发必须要掌握的技能，我会详细介绍position的4种状态并且用实例演示如何使用他们。
 <br/>
@@ -31,5 +33,6 @@ tags: [CSS]
 
         fixed是指元素脱离文档流，并且位置相对视窗固定，这就意味着滚动页面不会影响该元素的位置和显示。他同样有四个属性进行调整他所相对视窗边距的位移。
 
-<br/>
-    如果你对我的描述感到困惑，不妨看我的一个示例，里面清晰简洁的使用了position这个属性。<a target="_blank" href='http://jsfiddle.net/Alex___Yang/f9pvsdsc/'>如果你打不开，你就土了</a>
+
+
+如果你对我的描述感到困惑，不妨看我的一个示例，里面清晰简洁的使用了position这个属性。[如果你打不开，你就土了](http://jsfiddle.net/Alex___Yang/f9pvsdsc/)
--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,9 +1,12 @@
-$(document).ready(function(){
-if(window.location.href=="http://yeahyangliu.github.io/")
-{
-$("#sidebar").css({width:'100%'});
-$("#btnblog").click(function(){
-$("#sidebar").animate({width:'33.3333%'},'slow');
-});
-}
+$(function () {
+    $("#sidebar").addClass("img-" + Math.round((Math.random() * 2 + 1)));
+
+    if (window.location.href == "http://yeahyangliu.github.io/") {
+        $("#sidebar").css({width: '100%'});
+        $("#btnblog").click(function () {
+            $("#sidebar").animate({width: '33.3333%'}, 'slow');
+        });
+    }
+
+    $('#article_body a').attr("target", "_blank");
 });
